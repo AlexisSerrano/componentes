9a87d7b069b4970cf842021199cc6748289dd0db
Felipe <wixard43@gmail.com>
2018-05-15 16:11:30 -0500

    seeders de tablas para componentes

---
+++ database/seeds/sistemasSeeder.php
@@ -0,0 +1,17 @@
+<?php
+namespace fge\apis\seeds;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class sistemasSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('sistemas')->insert([
+            ['id'=>1,'nombre'=>'UIPJ','descripcion'=>'Unidad de investigación de la procuraduria'],
+            ['id'=>2,'nombre'=>'UAT(Unidad de Atención temprana)','descripcion'=>'Sistema orientado a atender las '],
+            ['id'=>3,'nombre'=>'VRR(Vehículos Robados y Recuperados)','descripcion'=>'Sistema enfocado a atender asuntos con vehículos '],
+            ['id'=>4,'nombre'=>'Periciales','descripcion'=>'Ssitema periciales'],
+            ['id'=>5,'nombre'=>'Desaparecidos','descripcion'=>'Sistema orientado a las personas que se encuentran en paradero desconocido']
+        ]);
+    }
+}
