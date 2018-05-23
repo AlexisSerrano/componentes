782b77b708d84f706e8fd5a578237c531ed5047c
Ortega Yañez Brain Rafael <yorb1990@gmail.com>
2018-05-23 14:43:00 -0500

    datatable updated

--- resources/views/DT.blade.php
+++ resources/views/DT.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="es">
   <head>
-    <title>Persona</title>
+    <title>DataTable</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
