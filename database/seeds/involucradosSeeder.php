1709f0bf6ae3f1020a99ce84c3dea229fc759762
ADFGE\wburgos <wburgos@fiscaliaveracruz.gob.mx>
2018-06-06 17:49:50 -0500

    Se agrega combinación 1,1,4 y 1,2,4 en ligadosseeder e involucradosseeder

--- database/seeds/involucradosSeeder.php
+++ database/seeds/involucradosSeeder.php
@@ -6,14 +6,16 @@ class involucradosSeeder extends Seeder
     public function run()
     {
         DB::table('involucrados')->insert([
-            ['id'=>1,'nombre'=>'comparecencia completo','descripcion'=>''],
-            ['id'=>2,'nombre'=>'conocido parcial','descripcion'=>''],
-            ['id'=>3,'nombre'=>'qrr null','descripcion'=>''],
-            ['id'=>10,'nombre'=>'comparecencia completo','descripcion'=>''],
-            ['id'=>11,'nombre'=>'conocido parcial','descripcion'=>''],
-            ['id'=>12,'nombre'=>'qrr null','descripcion'=>'']
+            ['id'=>1,'nombre'=>'comparecencia completo','descripcion'=>'UAT'],
+            ['id'=>2,'nombre'=>'conocido parcial','descripcion'=>'UAT'],
+            ['id'=>3,'nombre'=>'qrr null','descripcion'=>'UAT'],
+            ['id'=>4,'nombre'=>'victima u ofendido','descripcion'=>'UAT - UIPJ'],
+            ['id'=>10,'nombre'=>'comparecencia completo','descripcion'=>'UIPJ'],
+            ['id'=>11,'nombre'=>'conocido parcial','descripcion'=>'UIPJ'],
+            ['id'=>12,'nombre'=>'qrr null','descripcion'=>'UIPJ']
 
-            /*['id'=>1,'nombre'=>'victima u ofendido','descripcion'=>'Aquella persona que recibe el daño o se ve afectada por el denunciado'],
+            /*
+            ['id'=>1,'nombre'=>'victima u ofendido','descripcion'=>'Aquella persona que recibe el daño o se ve afectada por el denunciado'],
             ['id'=>4,'nombre'=>'Investigado(Comparecencia)','descripcion'=>'Persona que es objeto de investigacion y que está presente al momento del registro'],
             ['id'=>5,'nombre'=>'Autoridad','descripcion'=>'Persona que tiene la facultdad de mandar a las personas que estén subordinadas'],
             ['id'=>6,'nombre'=>'Testigo','descripcion'=>'Persona que ha presenciado un hecho determinado y que puede dar un testimonio'],
@@ -21,7 +23,8 @@ class involucradosSeeder extends Seeder
             ['id'=>8,'nombre'=>'Persona','descripcion'=>'Aquella persona que ocupa el mayor número de registros provenientes de la tabla persona'],
             ['id'=>9,'nombre'=>'Denunciante','descripcion'=>'Persona que interpone una denuncia'],
             ['id'=>12,'nombre'=>'Denunciado(Comparecencia)','descripcion'=>'Presunto responsable de los hechos y que está presente'],
-            ['id'=>13,'nombre'=>'Persona Moral','descripcion'=>'Datos de empresa o persona moral']*/
+            ['id'=>13,'nombre'=>'Persona Moral','descripcion'=>'Datos de empresa o persona moral']
+            */
         ]);
     }
 }
