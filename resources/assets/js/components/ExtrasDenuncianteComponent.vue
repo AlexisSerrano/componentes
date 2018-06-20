e33cf95768beb28de3e6af56a7f22ca8d5f99c7c
Felipe <wixard43@gmail.com>
2018-06-20 11:18:16 -0500

    Mejoras en extra denunciante

--- resources/assets/js/components/ExtrasDenuncianteComponent.vue
+++ resources/assets/js/components/ExtrasDenuncianteComponent.vue
@@ -5,27 +5,26 @@
 
     <div class="form-row">
 
-	    <div class="form-group col-md-3">		
-            <label class="col-form-label col-form-label-sm" for="identidad"> Identidad resguardada </label>           		
-            <v-select :options="ident" label="nombre" v-model="identidad" name="identidad" v-validate="'required'" :class="{'border border-danger rounded': errors.has('identidad')}" placeholder="Seleccione" ></v-select>
-            <span v-show="errors.has('identidad')" class="text-danger">{{ errors.first('identidad')}}</span>            
-	    </div>
+   <div class="form-group col-md-3">
+        <label class="col-form-label col-form-label-sm" for="identidad">Identidad resguardada</label>    
+        <v-select :options="ident" label="identidad" v-model="identidad" name="identidad" v-validate="'required'" :class="{ 'border border-danger rounded': errors.has('identidad') || this.validacionesback.periodo}"></v-select>
+        <span v-show="errors.has('identidad')" class="text-danger">{{ errors.first('identidad')}}</span>
+        <span v-if="this.validacionesback.periodo!=undefined" class="text-danger">{{ String(this.validacionesback.periodo)}}</span>
+    </div>
 
+    <div class="form-group col-md-3">
+        <label class="col-form-label col-form-label-sm" for="tipoSolicitante">Tipo de solicitante</label>    
+        <v-select :options="solicitantes" label="nombre" v-model="tipoSolicitante" name="tipoSolicitante" v-validate="'required'" :class="{ 'border border-danger rounded': errors.has('tipoSolicitante') || this.validacionesback.periodo}" ></v-select>
+        <span v-show="errors.has('tipoSolicitante')" class="text-danger">{{ errors.first('tipoSolicitante')}}</span>
+        <span v-if="this.validacionesback.periodo!=undefined" class="text-danger">{{ String(this.validacionesback.periodo)}}</span>
+    </div>
 
-<div class="form-group col-md-3">		
-            <label class="col-form-label col-form-label-sm" for="identidad"> Tipo de solicitante </label>           		
-            <v-select :options="ident" label="nombre" v-model="identidad" name="identidad" v-validate="'required'" :class="{'border border-danger rounded': errors.has('identidad')}" placeholder="Seleccione" ></v-select>
-            <span v-show="errors.has('identidad')" class="text-danger">{{ errors.first('identidad')}}</span>            
-</div>
-
-	<div class="col-12">
-		<div class="form-group">
-            <label class="col-form-label-sm">Descripcion de los hechos</label>
-            <textarea v-model="descripcionHechos" cols="30" rows="5" class="form-control form-control-sm" placeholder="Ingrese la descripcón de los hechos"></textarea>
-		</div>
-	</div>
-
-
+       <div class="form-group col-md-12">
+            <label class="col-form-label col-form-label-sm" for="descripcionHechos">Señas particulares</label>
+            <textarea class="form-control form-control-sm" cols="30" rows="5" name="descripcionHechos" :class="{'input': true, 'form-control':true, 'border border-danger': errors.has('descripcionHechos') || this.validacionesback.particulares}" v-model="descripcionHechos" placeholder="Ingrese la descripcion de los hechos" v-validate="'required'" autocomplete="off"></textarea>
+            <span v-show="errors.has('descripcionHechos')" class="text-danger">{{ errors.first('descripcionHechos')}}</span>
+            <span v-if="this.validacionesback.particulares!=undefined" class="text-danger">{{ String(this.validacionesback.particulares)}}</span>
+        </div>
 </div>
  </form>
 </div>
@@ -35,15 +34,13 @@
 import swal from 'sweetalert2'
     export default {
         data(){
-             return{
-                identidadRes: '',
-                tipoSolicitante: '',
-                descripcionHechos:'',  
+             return{                                                
                 ident:['No','Si'],
-                validacionesback:[],
-                identidad:'',
-                //url:'/api'
-                url:'http://localhost/componentes/public/api'
+                identidad:'',                
+                solicitantes:['Victima','Ofendido'],
+                tipoSolicitante:'',
+                descripcionHechos:'',
+                validacionesback:[]
             }
         },
         props:{
