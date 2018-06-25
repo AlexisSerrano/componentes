8eb49a688c2696bf388ec5a019d328f60ceb06e0
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-06-25 17:55:34 -0500

    default en tipo determinacion

--- database/migrations/2018_05_15_192229_create_apariciones_table.php
+++ database/migrations/2018_05_15_192229_create_apariciones_table.php
@@ -21,7 +21,7 @@ class CreateAparicionesTable extends Migration
             $table->string('tipoInvolucrado',20);
             $table->string('nuc',50);
             $table->boolean('esEmpresa')->nullable()->default(false);
-            $table->integer('idTipoDeterminacion')->unsigned();
+            $table->integer('idTipoDeterminacion')->unsigned()->default(1);
             //$table->integer('idStatusCarpeta')->unsigned();         
             $table->timestamps();
             $table->softDeletes();
