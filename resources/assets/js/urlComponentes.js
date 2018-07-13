2ede9f24504cb6002fc6b22ba1b9ddca92724d00
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-07-13 11:03:03 -0500

    de

--- resources/assets/js/urlComponentes.js
+++ resources/assets/js/urlComponentes.js
@@ -1,2 +1,3 @@
-const urlComponentes = 'http://componentes.fiscaliaveracruz.gob.mx/api'
+//const urlComponentes = 'http://componentes.fiscaliaveracruz.gob.mx/api'
+const urlComponentes = 'http://localhost/componentes/public/api'
 export default urlComponentes
