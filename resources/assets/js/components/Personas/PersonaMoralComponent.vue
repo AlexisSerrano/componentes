a0a3e7b303800b2140dd2d684df2e4b2fdef7afe
ADFGE\aserrano <serrano.321@hotmail.com>
2018-07-13 09:25:21 -0500

    Movimientos moral

--- resources/assets/js/components/Personas/PersonaMoralComponent.vue
+++ resources/assets/js/components/Personas/PersonaMoralComponent.vue
@@ -14,7 +14,7 @@
                 </div>
                 <div v-if="this.tipo!='conocidomoral'" :class="'form-group col-md-4'">
                     <label class="col-form-label col-form-label-sm" for="fechaCreacion">Fecha de creación</label>
-                    <input class="form-control form-control-sm" type="date" v-model="fechaCreacion" name="fechaCreacion" data-vv-name="fecha de creación" v-validate="'date_format:YYYY-MM-DD|before:' + today" :class="{ 'border border-danger': errors.has('fecha de creación') || this.validacionesback.fechaCreacion}"
+                    <input class="form-control form-control-sm" type="date" v-model="fechaCreacion" name="fechaCreacion" data-vv-name="fecha de creación" v-validate="'required|date_format:YYYY-MM-DD|before:' + today" :class="{ 'border border-danger': errors.has('fecha de creación') || this.validacionesback.fechaCreacion}"
                         @blur="calcularRfc" :readonly="this.$store.state.moralEncontrada==true">
                     <span v-show="errors.has('fecha de creación')" class="text-danger">{{ errors.first('fecha de creación')}}</span>
                     <span v-if="this.validacionesback.fechaCreacion!=undefined" class="text-danger">{{ String(this.validacionesback.fechaCreacion)}}</span>
