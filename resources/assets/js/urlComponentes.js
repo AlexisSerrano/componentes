18f9def2f33f3e263dd2b4345e9a1ec2d0cd9761
ADFGE\aserrano <serrano.321@hotmail.com>
2018-07-13 11:00:51 -0500

    favicon

--- resources/assets/js/urlComponentes.js
+++ resources/assets/js/urlComponentes.js
@@ -1,2 +1,3 @@
 const urlComponentes = 'http://componentes.fiscaliaveracruz.gob.mx/api'
+// const urlComponentes = 'http://localhost/public/api'
 export default urlComponentes
--- resources/views/registroActasHechos.blade.php
+++ resources/views/registroActasHechos.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{asset('css/app.css')}}" rel="stylesheet" />  
-        
+    <link rel="icon" href="{{ asset('img/logo.png') }}">
   </head>
   <body>
 	    <div id="app">
