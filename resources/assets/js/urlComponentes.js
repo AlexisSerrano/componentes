f53714ca3af831d7e6019a2dc78eff38ee6f033a
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-07-19 13:08:45 -0500

    con ruta local

--- resources/assets/js/urlComponentes.js
+++ resources/assets/js/urlComponentes.js
@@ -1,3 +1,3 @@
- const urlComponentes = 'http://sistemas.fiscaliaveracruz.gob.mx/componentes/public/api'
-//const urlComponentes = 'http://localhost/componentes/public/api'
+//const urlComponentes = 'http://sistemas.fiscaliaveracruz.gob.mx/componentes/public/api'
+const urlComponentes = 'http://localhost/componentes/public/api'
 export default urlComponentes
