3eed450970e74d3a47d2341b889deae60d2fbba2
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-07-27 08:40:37 -0500

    con extras

--- app/Http/Controllers/ValidacionEditController.php
+++ app/Http/Controllers/ValidacionEditController.php
@@ -8,6 +8,10 @@ use App\Http\Models\VariablesPersonaMoral;
 use App\Http\Models\aparicionesModel;
 use App\Http\Models\ExtraDenunciadoFisico;
 use App\Http\Models\ExtraDenunciadoMoral;
+use App\Http\Models\ExtraDenuncianteFisico;
+use App\Http\Models\ExtraDenuncianteMoral;
+use App\Http\Models\ExtraAbogado;
+use App\Http\Models\ExtraAutoridad;
 use App\Http\Controllers\PersonaController;
 use DB;
 
@@ -35,64 +39,51 @@ class ValidacionEditController extends Controller
 			case 'denunciadomoral':
 				$datos = ValidacionEditController::getDenunciadoMoral($request);
 				break;
-			// case 'conocido':
-			// 	$datos = ValidacionEditController::getConocidoFisico($request);
-			// 	break;
-			// case 'conocidomoral':
-			// 	$datos = ValidacionEditController::getConocidoMoral($request);
-			// 	break;
-    
         }
         return $datos;
     }
 
     public function getAutoridad(Request $request){
         $data['persona'] = ValidacionEditController::getPersonaCompleta($request);
-        $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['extra'] = ValidacionEditController::getExtraAutoridad($request->idVarPersona);
 		return response()->json($data);
     }
 
     public function getAbogado(Request $request){
         $data['persona'] = ValidacionEditController::getPersonaCompleta($request);
-        $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['extra'] = ValidacionEditController::getExtraAbogado($request->idVarPersona);
 		return response()->json($data);
     }
 
     public function getDenuncianteFisico(Request $request){
         $data['persona'] = ValidacionEditController::getPersonaCompleta($request);
-        $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['extra'] = ValidacionEditController::getExtraDenuncianteFisico($request->idVarPersona);
 		return response()->json($data);
     }
 
     public function getDenuncianteMoral(Request $request){
         $data['persona'] = ValidacionEditController::getPersonaMoralCompleta($request);
-        $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['extra'] = ValidacionEditController::getExtraDenuncianteMoral($request->idVarPersona);
 		return response()->json($data);
     }
 
     public function getDenunciadoFisico(Request $request){
         $data['persona'] = ValidacionEditController::getPersonaCompleta($request,1);
-        $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['extra'] = ValidacionEditController::getExtraDenunciadoFisico($request->idVarPersona);
 		return response()->json($data);
     }
 
     public function getDenunciadoMoral(Request $request){
         $data['persona'] = ValidacionEditController::getPersonaMoralCompleta($request);
-        $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['domicilios'] = PersonaController::getDomiciliosPersona($request);
+		$data['extra'] = ValidacionEditController::getExtraDenunciadoMoral($request->idVarPersona);
 		return response()->json($data);
     }
-
-    // public function getConocidoFisico(Request $request){
-	// 	$data['persona'] = ValidacionEditController::getPersonaFisicaConocido($request);
-    //     $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
-	// 	return response()->json($data);
-    // }
-
-    // public function getConocidoMoral(Request $request){
-	// 	$data['persona'] = ValidacionEditController::getPersonaMoralConocido($request);
-    //     $data['domicilios'] = PersonaController::getDomiciliosPersona($request);
-	// 	return response()->json($data);
-    // }
 
     public function getPersonaCompleta($request,$getalias=0){
         $idVarPersona = $request->idVarPersona;
@@ -220,67 +211,34 @@ class ValidacionEditController extends Controller
 		}
 		return response()->json($data);
     }
-    
-    // public function getPersonaMoralConocido(Request $request){
-	// 	$idVarPersona = $request->idVarPersona;
-	// 	$personaExiste = DB::table('persona_moral')
-	// 	->join('variables_persona_moral', 'variables_persona_moral.idPersona', '=', 'persona_moral.id')
-	// 	->where('variables_persona_moral.id',$idVarPersona)
-    //     ->select('persona_moral.id as id',
-    //     'persona_moral.nombre',
-	// 	'variables_persona_moral.id as idVar',
-	// 	'variables_persona_moral.idDomicilio',
-	// 	'variables_persona_moral.idNotificacion')
-	// 	->orderBy('variables_persona_moral.id','desc')
-	// 	->first();
-	// 	if($personaExiste){
-	// 		$data = array(
-	// 			'nombre'=>$personaExiste->nombre,
-	// 			'idMoral'=>$personaExiste->id,
-	// 			'idPersona'=>$personaExiste->id,
-	// 			'idVarPersona'=>$personaExiste->idVar,
-	// 			'idDomicilio'=>$personaExiste->idDomicilio,
-	// 			'idDomicilioNotificacion'=>$personaExiste->idNotificacion
-	// 		);
-	// 	}else{
-	// 		$data = array(
-	// 		);
-	// 	}
-	// 	return response()->json($data);
-	// }
+	
+	public function getExtraDenunciadoFisico($id){
+		$extra = ExtraDenunciadoFisico::where('idVariablesPersona',$id);
+		return response()->json($extra);
+	}
 
-	// public function getPersonaFisicaConocido($request){
-    //     $idVarPersona = $request->idVarPersona;
-	// 	$personaExisteP = DB::table('persona_fisica')
-	// 	->join('variables_persona_fisica', 'variables_persona_fisica.idPersona', '=', 'persona_fisica.id')
-	// 	->where('variables_persona_fisica.id',$idVarPersona)
-	// 	->select('persona_fisica.id as id','persona_fisica.nombres','persona_fisica.primerAp','persona_fisica.segundoAp',
-	// 	'variables_persona_fisica.id as idVar','variables_persona_fisica.idDomicilio','variables_persona_fisica.idTrabajo',
-	// 	'variables_persona_fisica.idNotificacion')
-	// 	->orderBy('variables_persona_fisica.id','desc')
-	// 	->first();
-	// 	$alias = DB::table('extra_denunciado_fisico')
-	// 	->select('alias','id')
-	// 	->where('idVariablesPersona',$personaExisteP->idVar)
-	// 	->first();
-    //     if($personaExisteP){
-	// 		$data = array(
-	// 			'nombres'=>$personaExisteP->nombres,
-	// 			'primerAp'=>$personaExisteP->primerAp,
-	// 			'segundoAp'=>$personaExisteP->segundoAp,
-	// 			'idDomicilio'=>$personaExisteP->idDomicilio,
-	// 			'idDomicilioTrabajo'=>$personaExisteP->idTrabajo,
-	// 			'idDomicilioNotificacion'=>$personaExisteP->idNotificacion,
-	// 			'idPersona'=>$personaExisteP->id,
-	// 			'idVarPersona'=>$personaExisteP->idVar,
-	// 			'alias'=>$alias->alias,
-	// 			'idExtra'=>$alias->id
-	// 		);
-	// 	}
-	// 	else{
-	// 		$data = array(
-	// 		);
-	// 	}
-	// 	return response()->json($data);
-    // }
+	public function getExtraDenunciadoMoral($id){
+		$extra = ExtraDenunciadoMoral::where('idVariablesPersona',$id);
+		return response()->json($extra);
+	}
+
+	public function getExtraDenuncianteFisico($id){
+		$extra = ExtraDenuncianteFisico::where('idVariablesPersona',$id);
+		return response()->json($extra);
+	}
+
+	public function getExtraDenuncianteMoral($id){
+		$extra = ExtraDenuncianteMoral::where('idVariablesPersona',$id);
+		return response()->json($extra);
+	}
+
+	public function getExtraAutoridad($id){
+		$extra = ExtraAutoridad::where('idVariablesPersona',$id);
+		return response()->json($extra);
+	}
+
+	public function getExtraAbogado($id){
+		$extra = ExtraAbogado::where('idVariablesPersona',$id);
+		return response()->json($extra);
+	}
 }
