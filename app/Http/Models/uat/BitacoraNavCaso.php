1bb2579de2f579bc49d33c7b3ba3ec3788b63055
ADFGE\cserrano <kayser.fb@hotmail.com>
2018-07-30 13:05:11 -0500

    conexion a uat

---
+++ app/Http/Models/uat/BitacoraNavCaso.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\uat;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BitacoraNavCaso extends Model
+{
+    protected $connection = 'uat';
+    public $table = 'bitacora_navcaso';
+
+    public $fillable = [
+        'id',
+        'denunciante',
+        'denunciado',
+        'abogado',
+        'autoridad',
+        'delitos',
+        'acusaciones',
+        'defensa',
+        'hechos',
+        'medidas',
+        'idCaso',
+        'created_at',
+        'updated_at'
+    ];
+}
--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'uat' => [
+            'driver' => env('DB_CONNECTION_UAT'),
+            'host' => env('DB_HOST_UAT'),
+            'port' => env('DB_PORT_UAT'),
+            'database' => env('DB_DATABASE_UAT'),
+            'username' => env('DB_USERNAME_UAT'),
+            'password' => env('DB_PASSWORD_UAT'),
+            'unix_socket' => env('DB_SOCKET'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '192.108.24.142'),
